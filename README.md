da11d82cff2748f7ea5bac085bde9a0efb870f65
Jason Gibson <65173587+JGibson37@users.noreply.github.com>
2021-07-01 18:45:53 -0400

    Finished Logic
    I realized in past commits I misunderstood the prompt.
    -added a List
    -changed variable names to better suit their actual purpose
    -added break to prevent out of bounds
    -added logic to store numbers and swap their positions according to K
    -added logic to convert the ints back into a string and save them in the list
    -print out List for final answer in [num, num, num,...] format.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # reverseGroups
+  Programming challenge description:
+ Given a list of numbers and a positive integer k, reverse the elements of the list, k items at a time. 
+ If the number of elements is not a multiple of k, then the remaining items in the end should be left as is.
  
+ Input:
+ Your program should read lines from standard input. Each line contains a list of numbers and the number k, 
+ separated by a semicolon. The list of numbers are comma delimited.
+ 
+ Output:
+ Print out the new comma separated list of numbers obtained after reversing.
+ 
+ Test 1
+ Test Input
+ 1,2,3,4,5;2
+ 
+ Expected Output
+ 2,1,4,3,5
+ 
+ Test 2
+ Test Input
+ 1,2,3,4,5;3
+ 
+ Expected Output
+ 3,2,1,4,5
--- src/main/java/reverseStringApp.java
+++ src/main/java/reverseStringApp.java
@@ -3,31 +3,39 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 
 public class reverseStringApp {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\WeCanCodeIT\\Documents\\reverseGroups\\src\\test\\java\\Test1.text"));
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\jason\\IdeaProjects\\reverseGroups\\src\\test\\java\\Test1.text"));
         String input;
 
-        while((input = br.readLine()) != null){
+        while ((input = br.readLine()) != null) {
             String[] firstSplit = input.split(";");
             String[] lineNums = firstSplit[0].split(",");
+            int k = Integer.parseInt(firstSplit[1]);
             int[] numbers = new int[5];
+            List<String> numList = new ArrayList<>();
 
-            for(int y=0; y<lineNums.length; y++){
-                int k = Integer.parseInt(firstSplit[1]);
-                numbers[y] = Integer.parseInt(lineNums[y]);
-                if(numbers[y] % k == 0){
-                    int numToMove = numbers[y];
-                    int numToSave = numbers[y-1];
-                    numbers[y] = numToSave;
-                    numbers[y-1] = numToMove;
+            for (int z = 0; z < lineNums.length; z++) {
+                numbers[z] = Integer.parseInt(lineNums[z]);
+            }
+
+            for (int currentPosition = 0; currentPosition < numbers.length; currentPosition = currentPosition + k) {
+                if(currentPosition+k >= numbers.length){
+                    break;
                 }
-            }
+                int currentNumber = numbers[currentPosition];
+                int numberToSwapWith = numbers[currentPosition + (k-1)];
+                numbers[currentPosition + (k-1)] = currentNumber;
+                numbers[currentPosition] = numberToSwapWith;
 
-            for(int x=0; x < numbers.length; x++){
-                System.out.println(numbers[x]);
             }
+            for (int x = 0; x < 5; x++) {
+                numList.add(String.valueOf(numbers[x]));
+            }
+            System.out.println(numList);
         }
     }
 }
